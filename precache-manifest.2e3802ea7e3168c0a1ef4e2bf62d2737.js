93d527a37402bebed2c93ba130e51d64db5e721d
abaasmuhidiin <32631252+abaasmuhidiin@users.noreply.github.com>
2020-05-20 16:20:08 +0200

    Updates

---
+++ precache-manifest.2e3802ea7e3168c0a1ef4e2bf62d2737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7bf595044a8918708689460338e722",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "edefe8d90801f1014eae",
+    "url": "/robofriends/static/css/main.287408d7.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edefe8d90801f1014eae",
+    "url": "/robofriends/static/js/main.750989ae.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
